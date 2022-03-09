db2dcb99242cb69cc4c7ee9b4e9464d494d26340
soucre.thaipvt <soucre.thaipvt@gmail.com>
2022-03-09 15:00:17 +0700

    Setup Libary Chart-Kit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './src/containers/App';
+import Test from './src/containers/testLibary';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Test);
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
