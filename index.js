031ba2ddf1e14b8b34991a7267b293af02b5cce2
soucre.thaipvt <soucre.thaipvt@gmail.com>
2022-03-09 15:16:39 +0700

    fix moudle libary Chart

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
@@ -7,4 +8,4 @@ import App from './src/containers/App';
 import Test from './src/containers/testLibary';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Test);
+AppRegistry.registerComponent(appName, () => App);
